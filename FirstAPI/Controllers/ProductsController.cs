0257ea15193f9de873853e76eec25c8195a907c5
MDABDULMALIK121 <abdulmalikjmi6@gmail.com>
2022-03-24 16:54:55 +0530

    Add project files.

---
+++ FirstAPI/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstAPI.Models;
+
+namespace FirstAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ApplicationDbContext applicationDb;
+        public ProductsController(ApplicationDbContext applicationDb)
+        {
+            this.applicationDb = applicationDb;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var products = applicationDb.Products.ToList();
+           
+                return Ok(products);
+           
+
+        }
+        [HttpPost]
+        public IActionResult Post(Products products)
+        {
+            applicationDb.Products.Add(products);
+            applicationDb.SaveChanges();
+            return Ok(new { ok = true, message = "product created successfully...." });
+        }
+    }
+}
---
+++ FirstAPI/Models/Products.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstAPI.Models
+{
+    public class Products
+    {
+        public int Id{ get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public int quantity { get; set; }
+        public string description { get; set; }
+    }
+}
